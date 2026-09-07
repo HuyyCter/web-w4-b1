ef3fdfd194e3161d3c3c1a720f4d521fecea25c3
HuyyCter <nguyenquanghuy070306@gmail.com>
2026-09-07 19:41:04 +0700

    first commit

---
+++ src/main/java/controller/EmailListServlet.java
@@ -0,0 +1,72 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.User;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class EmailListServlet
+ */
+@WebServlet("/emailList")
+public class EmailListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmailListServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String url = "/index.jsp";
+
+        // get current action
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "join";  // default action
+        }
+        // perform action and set URL to appropriate page
+        if (action.equals("join")) {
+            url = "/index.jsp";    // the "join" page
+        }
+        else if (action.equals("add")) {                
+            // get parameters from the request
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+            String email = request.getParameter("email");
+
+            // store data in User object and save User object in db
+            User user = new User(firstName, lastName, email);
+            // UserDB.insert(user);
+            
+            // set User object in request object and set URL
+            request.setAttribute("user", user);
+            url = "/thanks.jsp";   // the "thanks" page
+        }
+        
+        // forward request and response objects to specified URL
+        getServletContext()
+            .getRequestDispatcher(url)
+            .forward(request, response);
+	}
+
+}
